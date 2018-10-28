a07ab9271f1d40889352208bacfa44e74fd75bd9
Paul Kubala <paulkubala@Pauls-MacBook-Pro.local>
2018-10-28 12:02:02 -0700

    moving space into our component

---
+++ tic-tac-toe/src/components/space/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class Space extends Component {
+  render() {
+    return (
+      <div className="space">X</div>
+    );
+  }
+}
+
+export default Space;

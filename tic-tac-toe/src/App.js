c12691303863dc037014aabab068ec2081b70fc7
Paul Kubala <paulkubala@Pauls-MacBook-Pro.local>
2018-10-27 21:37:52 -0700

    adding a grid

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -4,7 +4,42 @@ import './App.scss';
 class App extends Component {
   render() {
     return (
-        <p className="test">X</p>
+      <div>
+        <div className="row">
+          <div className="col">
+            <span className="content">X</span>
+          </div>
+          <div className="col">
+            <span className="content">X</span>
+          </div>
+          <div className="col">
+            <span className="content">X</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <span className="content">X</span>
+          </div>
+          <div className="col">
+            <span className="content">X</span>
+          </div>
+          <div className="col">
+            <span className="content">X</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <span className="content">X</span>
+          </div>
+          <div className="col">
+            <span className="content">X</span>
+          </div>
+          <div className="col">
+            <span className="content">X</span>
+          </div>
+        </div>
+      </div>
+
     );
   }
 }

60137aa39d40cc191a24094f8691720f5aae4c8d
Paul Kubala <paulkubala@Pauls-MacBook-Pro.local>
2018-10-28 14:49:23 -0700

    fixing error and adjusting borad building

--- tic-tac-toe/src/components/board/index.js
+++ tic-tac-toe/src/components/board/index.js
@@ -18,35 +18,28 @@ class Board extends Component {
   }
   createSpace = (i) => {
     return(
-      <Space value={this.state.spaces[i]} onClick={this.handleClick(i)}></Space>
+      <Space value={this.state.spaces[i]} onClick={() => this.handleClick(i)}></Space>
     )
   }
-  createRows = () => {
+  createRows = (rowAdjustment) => {
     let rows = [];
-    for(let i = 0; i < 9; i++) {
-        rows.push(<div key={i} className="col">{this.createSpace(i)}</div>);
+    rowAdjustment = (rowAdjustment*3);
+    for(let i = 0; i < 3; i++) {
+        rows.push(<div key={i+rowAdjustment} className="col">{this.createSpace(i+rowAdjustment)}</div>);
       }
     return rows;
   }
-  // createBoard = () => {
-  //   let board = [];
-  //   for(let i = 0; i < 3; i++) {
-  //     board.push(<div key={i} className="row">{this.createRows()}</div>);
-  //   }
-  //   return board;
-  // }
+  createBoard = () => {
+    let board = [];
+    for(let i = 0; i < 3; i++) {
+      board.push(<div key={i} className="row">{this.createRows(i)}</div>);
+    }
+    return board;
+  }
   render() {
     return (
       <div className="board">
-        {this.createSpace(0)}
-        {this.createSpace(1)}
-        {this.createSpace(2)}
-        {this.createSpace(3)}
-        {this.createSpace(4)}
-        {this.createSpace(5)}
-        {this.createSpace(6)}
-        {this.createSpace(7)}
-        {this.createSpace(8)}
+      {this.createBoard()}
       </div>
     );
   }

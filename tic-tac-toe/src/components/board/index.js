caa06bb05dfae2875de98d30677809f64749e79d
Paul Kubala <paulkubala@Pauls-MacBook-Pro.local>
2018-10-28 14:41:34 -0700

    still errors trying to figure out what I missed moving state to board

--- tic-tac-toe/src/components/board/index.js
+++ tic-tac-toe/src/components/board/index.js
@@ -11,34 +11,42 @@ class Board extends Component {
     };
   }
 
-  handleClick = () => {
-    if(!this.state.value){
-      this.setState({value: 'X'});
-    }
+  handleClick = (i) => {
+    const spaces = this.state.spaces.slice();
+    spaces[i] = 'X';
+    this.setState({spaces: spaces});
   }
   createSpace = (i) => {
     return(
-      <Space value={this.state.spaces[i]} onClick={this.handleClick()}></Space>
+      <Space value={this.state.spaces[i]} onClick={this.handleClick(i)}></Space>
     )
   }
   createRows = () => {
     let rows = [];
-    for(let i = 0; i < 3; i++) {
+    for(let i = 0; i < 9; i++) {
         rows.push(<div key={i} className="col">{this.createSpace(i)}</div>);
       }
     return rows;
   }
-  createBoard = () => {
-    let board = [];
-    for(let i = 0; i < 3; i++) {
-      board.push(<div key={i} className="row">{this.createRows()}</div>);
-    }
-    return board;
-  }
+  // createBoard = () => {
+  //   let board = [];
+  //   for(let i = 0; i < 3; i++) {
+  //     board.push(<div key={i} className="row">{this.createRows()}</div>);
+  //   }
+  //   return board;
+  // }
   render() {
     return (
       <div className="board">
-        {this.createBoard()}
+        {this.createSpace(0)}
+        {this.createSpace(1)}
+        {this.createSpace(2)}
+        {this.createSpace(3)}
+        {this.createSpace(4)}
+        {this.createSpace(5)}
+        {this.createSpace(6)}
+        {this.createSpace(7)}
+        {this.createSpace(8)}
       </div>
     );
   }

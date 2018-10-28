a686138b003fff832fd0a34ef89d9c248357acab
Paul Kubala <paulkubala@Pauls-MacBook-Pro.local>
2018-10-28 12:15:21 -0700

    wiring up a click

--- tic-tac-toe/src/components/space/index.js
+++ tic-tac-toe/src/components/space/index.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import './styles.scss';
 
 class Space extends Component {
+
+  handleClick() {
+    console.log('click yo');
+  }
   render() {
     return (
-      <div className="space">X</div>
+      <div className="space" onClick={this.handleClick}>X</div>
     );
   }
 }
+
 
 export default Space;

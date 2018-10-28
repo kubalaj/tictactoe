f90b98fd617bdfcacdaa595b0dfd99ad529a9cae
Paul Kubala <paulkubala@Pauls-MacBook-Pro.local>
2018-10-28 10:45:52 -0700

    adding component

---
+++ tic-tac-toe/src/components/board/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col">
+            <div className="content">X</div>
+          </div>
+          <div className="col">
+            <div className="content">X</div>
+          </div>
+          <div className="col">
+            <div className="content">X</div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="content">X</div>
+          </div>
+          <div className="col">
+            <div className="content">X</div>
+          </div>
+          <div className="col">
+            <div className="content">X</div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="content">X</div>
+          </div>
+          <div className="col">
+            <div className="content">X</div>
+          </div>
+          <div className="col">
+            <div className="content">X</div>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default App;

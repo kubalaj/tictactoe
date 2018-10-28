c921094379cdec4cc47c29a3cdcd5c49681d15ad
Paul Kubala <paulkubala@Pauls-MacBook-Pro.local>
2018-10-28 15:28:00 -0700

    trying to add a simple GET

--- backend-ai/hello.py
+++ backend-ai/hello.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+test = [
+    {
+        'test': 'test'
+    }
+]
+
+@app.route('/tictactoe', methods=['GET'])
 def hello_world():
     return 'Hello, World!'

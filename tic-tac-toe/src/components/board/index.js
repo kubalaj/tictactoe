0be484eff4f6febfaff71da2b133e4dcda164552
Paul Kubala <paulkubala@Pauls-MacBook-Pro.local>
2018-11-01 11:53:06 -0700

    cleaning up reset a bit more

--- tic-tac-toe/src/components/board/index.js
+++ tic-tac-toe/src/components/board/index.js
@@ -40,8 +40,7 @@ class Board extends Component {
 
   async resetBoard() {
     await this.setStateAsync(this.baseState);
-    axios.get(`http://127.0.0.1:5000/api/reset`).then(res => {
-    });
+    axios.get(`http://127.0.0.1:5000/api/reset`);
   }
 
   async computerMove(move) {

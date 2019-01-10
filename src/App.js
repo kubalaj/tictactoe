233194e55b17cb8b8b90b2dddaf519009000b493
kubalaj <jp@jpkubala.com>
2019-01-09 18:20:49 -0800

    changing file structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+import Board from './components/board';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Board></Board>
+    );
+  }
+}
+
+export default App;
